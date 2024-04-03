20ff2c7517a4f9ee3184847cd2657da64dc54985
Viet Tung <Viettung150803@gmail.com>
2024-04-03 10:55:53 +0700

    mock bussiness git project

---
+++ app.js
@@ -0,0 +1,22 @@
+var isValid = function (s) {
+  const stack = [];
+
+  const match = {
+    ")": "(",
+    "]": "[",
+    "}": "{",
+  };
+
+  for (const ch of s) {
+    if (ch === "(" || ch === "[" || ch === "{") {
+      stack.push(ch);
+    } else {
+      const top = stack.pop();
+      if (top !== match[ch]) {
+        return false;
+      }
+    }
+  }
+
+  return !stack.length;
+};
